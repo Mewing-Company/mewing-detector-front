ac44bc6843490329d85fa2ab3b1c2408cc92b07e
x1vaan <ivanx467@gmail.com>
2024-02-23 20:47:15 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="w-full h-full">
+      <div className="flex flex-col h-full w-full items-center">
+        
+      </div>
+    </main>
+  );
+}
